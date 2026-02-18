902de95331f7000fc3dcf0fe3025d54c927f7594
Tarun <tarun.saini@varaisys.com>
2026-02-18 09:33:57 +0530

    (add) Add Scalar Documatation in the Fast api Project.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI()
+
 
 @app.get("/shipment")
 def get_shipment():
@@ -9,5 +11,13 @@ def get_shipment():
         "content": "Cricket Bat",
         "destination": "Modinagar",
         "source": "Delhi",
-        "status": "Out for Delivery"
+        "status": "Out for Delivery",
     }
+
+
+@app.get("/scalar", include_in_schema=False)
+def get_scalar():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title="Shipment API",
+    )
